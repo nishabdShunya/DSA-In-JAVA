c0d44deeb75d8c00cfd0595f74b24bbe86cd7305
Nishant Choubisa <nishantchoubisa92@gmail.com>
2023-12-04 13:06:44 +0530

    Add. Basic Recursion Example 2

--- Recursion/src/BasicRecursionExamples.java
+++ Recursion/src/BasicRecursionExamples.java
@@ -5,14 +5,27 @@ public class BasicRecursionExamples {
         System.out.print("Please enter the value of n: ");
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
+        // Linearly print 1 to n using Recursion
+        System.out.print("Normal: ");
         print(1, n);
+        // Line break
+        System.out.println();
+        // Linearly print n to 1 using Recursion
+        System.out.print("Reverse: ");
+        printReverse(n);
     }
-
-    public static void print(int i, int n){
+    private static void print(int i, int n){
         if(i > n){
             return;
         }
-        System.out.println(i);
+        System.out.print(i + " ");
         print(i+1, n);
+    }
+    private static void printReverse(int n) {
+        if(n < 1){
+            return;
+        }
+        System.out.print(n + " ");
+        printReverse(n-1);
     }
 }
